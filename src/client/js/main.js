4bccb95de24576dbe53ff502c9f9bf5931e18505
Douglas Maciel d'Auriol Souza <ddauriol@gmail.com>
2020-04-16 12:56:58 -0300

    SocketIO

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -5,27 +5,58 @@ var port_servidor = '5000'
 // Estabelecendo uma conexão
 var socket = io.connect('http://' + ip_servidor + ':' + port_servidor);
 
+// Variaveis gerais
+const input_user_name = document.getElementById('input_user_name')
+var user_name = input_user_name.value
+const success_alert = document.getElementById('success-alert')
+const btn_entrar = document.getElementById('btn_entrar')
+const alert_danger = document.getElementById('alert-danger')
+const msg_error = document.getElementById('msg_error')
+const msg_sucess = document.getElementById('msg_sucess')
+
 // Returno da confirmação da conexão, mostra um alerta na tela.
 socket.on('connect', function () {
-    success_alert = document.getElementById('success-alert')
+    msg_sucess.innerHTML = 'Sucesso! Jogador conectado'
     success_alert.style.display = "block"
     setTimeout(function () {
-        success_alert.style.display='none';
-    }, 3000);
+        success_alert.style.display = 'none';
+    }, 2000);
     return false;
 });
 
+socket.on('status_msg', function (msg) {
+    show_msg('status_msg', msg)
+})
+
+function show_msg(tipo='status_msg', msg=''){
+    if (tipo == 'status_msg'){
+        msg_sucess.innerHTML = msg
+        success_alert.style.display = "block"
+        setTimeout(function () {
+            success_alert.style.display = 'none';
+        }, 2000);
+        return false;
+    }
+    if (tipo == 'erro_msg'){
+        msg_sucess.innerHTML = msg
+        success_alert.style.display = "block"
+        setTimeout(function () {
+            success_alert.style.display = 'none';
+        }, 2000);
+        return false;
+    }
+}
 
 // Criando Grid
 var div_pricipal = document.getElementById("div_principal")
-function CreateGrid(){
+function CreateGrid() {
     var htmlOut = ''
     var htmlRow = ''
     i = 1
-    while (i < 22){
+    while (i < 22) {
         htmlRow = `<div class='row' id='row_${i}'>`
         j = 1
-        while (j < 22){
+        while (j < 22) {
             htmlRow += `<div class="gameGrid" id='grid_${i}_${j}'></div>`
             j++
         }
@@ -36,13 +67,27 @@ function CreateGrid(){
     div_pricipal.innerHTML = htmlOut
 }
 
+function InToGame() {
+    user_name = input_user_name.value
+    if (user_name != '') {
+        socket.emit('new_player', user_name)
+        input_user_name.disabled = 'disabled'
+        btn_entrar.disabled = 'disabled'
+    }
+}
+
 // Monitorando clicks
-document.addEventListener('click', function(e) {
+document.addEventListener('click', function (e) {
     e = e || window.event;
     var target = e.target || e.srcElement,
         TextId = target.id;
     console.log(TextId)
-    socket.emit('verifica_resposta', TextId)
+    if (TextId == 'btn_entrar'){
+        InToGame()
+    }
+    if(TextId.substring(0, 4) == 'grid'){
+        socket.emit('check_response', TextId + ';' + user_name)
+    }
 }, false);
 
 CreateGrid()
--- src/server/app.py
+++ src/server/app.py
@@ -20,23 +20,39 @@ responses = Responses(json_folder, json_response)
 
 # TEMP
 board.new_game(1)
+users.clear_users()
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @socketio.on('connect')
 def player_connected():
     emit('status_msg', "Conectado")
 
 
+@socketio.on('exit_game')
+def player_disconnected(user_name):
+    users.remove_user(user_name)
+    emit('exit_player',
+         {'msg': f"Usuário {user_name} se desconectou.",
+          'users': users.users,
+          'user_name': user_name},
+         broadcast=True)
+
+
 @socketio.on('new_player')
 def new_player(user_name):
+    user_name = user_name.upper()
     print(user_name)
     try:
         users.add_users(user_name, 0)
-        emit('status_msg', f"{user_name} entrou no jogo",
+        emit('status_msg', {'msg': f"{user_name} entrou no jogo",
+                            'users': users.users},
              broadcast=True)
+        emit('new_player', {'user_name': user_name})
     except EnvironmentError:
         emit('erro_msg', 'Impossível entrar no jogo')
 
@@ -62,11 +78,20 @@ def check_response(data):
     else:
         users.plus_points(user_name,
                           int(response['points']))
+        person = response['name']
         emit('results',
-             {'users': users.users,
+             {'msg': f'O jogador {user_name} achou o personagem {person}',
+              'users': users.users,
               'user': user_name,
-              'grid': grid},
+              'grid': grid,
+              'person': person},
              broadcast=True)
+
+
+@socketio.on('check_update')
+def check_update():
+    responses.get_response(board.active_place)
+    emit('board_updated', responses.active_response)
 
 
 if __name__ == '__main__':
