f3588e2cfe4448cbc8ef140a60ff09fea905fa53
Douglas Maciel d'Auriol Souza <ddauriol@gmail.com>
2020-04-15 22:52:10 -0300

    Retomado o porjeto

--- src/server/app.py
+++ src/server/app.py
@@ -1,43 +1,72 @@
-from flask import Flask, request, jsonify
+from threading import Lock
+from flask import Flask, request, jsonify, render_template
 from flask_socketio import SocketIO, send, emit
 
-import json
-import time
-
-from dbmanager import Places, Users
-from jsonmanager import Respostas
+from jsonmanager import Responses, Users, Board
 
 app = Flask(__name__)
+app.debug = True
 app.config['SECRET_KEY'] = 'whereswallyandfriends'
 socketio = SocketIO(app, cors_allowed_origins="*")
 
 json_folder = './json/'
-json_respostas = 'resposta.json'
+json_response = 'response.json'
 json_users = 'user.json'
-json_places = 'places.json'
+json_game = 'games.json'
 
-respostas = Respostas(json_folder, json_respostas)
-users = Users(json_folder + json_users)
-places = Places(json_folder + json_places)
-places.PurgeDB()
-places.CreatPlace()
+users = Users(json_folder, json_users)
+board = Board(json_folder, json_game)
+responses = Responses(json_folder, json_response)
 
+# TEMP
+board.new_game(1)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 @socketio.on('connect')
-def enviarDadosJogo():
-    emit('jogador_conectado', "")
+def player_connected():
+    emit('status_msg', "Conectado")
 
 
-@socketio.on('verifica_resposta')
-def verificaResposta(data):
-    print(data)
-    place = places.GetPlace()
-    for plc in place:
-        place_int = plc['active']
-    print(respostas.getResposta(place_int))
-    # emit('resultado',
-    #      {'room': room, 'msg': msg_retorno},
-    #      broadcast=True)
+@socketio.on('new_player')
+def new_player(user_name):
+    print(user_name)
+    try:
+        users.add_users(user_name, 0)
+        emit('status_msg', f"{user_name} entrou no jogo",
+             broadcast=True)
+    except EnvironmentError:
+        emit('erro_msg', 'Impossível entrar no jogo')
+
+
+@socketio.on('start_game')
+def start_game(place):
+    try:
+        board.new_game(int(place))
+        print('Jogo iniciado: place ' + str(place))
+        emit('started_game', board.active_place)
+    except EnvironmentError:
+        emit('erro_msg', 'Impossível iniciar o jogo')
+
+
+@socketio.on('check_response')
+def check_response(data):
+    grid, user_name = data.split(';')
+    print('O usuário: ' + user_name + ' clicou em: ' + grid)
+    responses.get_response(board.active_place)
+    response = responses.check_response(grid)
+    if response is None:
+        pass
+    else:
+        users.plus_points(user_name,
+                          int(response['points']))
+        emit('results',
+             {'users': users.users,
+              'user': user_name,
+              'grid': grid},
+             broadcast=True)
 
 
 if __name__ == '__main__':
--- src/server/jsonmanager.py
+++ src/server/jsonmanager.py
@@ -1,25 +1,117 @@
 import json
+import os
 
 
-class Respostas():
-    def __init__(self, json_folder, json_resposta):
-        self.json_folder = json_folder
-        self.json_resposta = json_resposta
+class Responses():
+    def __init__(self, json_folder: str, json_response: str):
+        self.json_folder: str = json_folder
+        self.json_response: str = json_response
+        self.file: str = self.json_folder + self.json_response
+        self.responses: list = []
+        self.active_response: list = []
 
-    def getResposta(self, place):
-        file = self.json_folder + self.json_resposta
-        with open(file, "r") as f:
-            data = json.load(f)
-            respostas = data
-        for resposta in respostas:
-            if resposta['place'] == place:
-                return resposta['person']
+    def get_response(self, place: int):
+        with open(self.file, "r") as f:
+            self.responses = json.load(f)
+        for response in self.responses:
+            if response['place'] == place:
+                self.active_response = response['person']
+
+    def check_response(self, grid: str):
+        for response in self.active_response:
+            if response['poss'] == grid:
+                return response
 
 
-# json_folder = './src/server/json/'
-# json_respostas = 'resposta.json'
-# respostas = Respostas(json_folder, json_respostas)
-# i = 1
-# while(i < 16):
-#     print(respostas.getResposta(i))
-#     i = i + 1
+class Users():
+    def __init__(self, json_folder: str, json_users: str):
+        self.json_folder: str = json_folder
+        self.json_users: str = json_users
+        self.file: str = self.json_folder + self.json_users
+        self.users: list = []
+
+    def file_exist(self):
+        if os.path.exists(self.file):
+            pass
+        else:
+            self.white_file()
+
+    def white_file(self):
+        with open(self.file, "w", encoding='utf-8') as f:
+            json.dump(self.users, f)
+
+    def check_exist_user(self, user_name: str):
+        self.get_users()
+        for user in self.users:
+            if user['user_name'] == user_name.upper():
+                return True
+        return False
+
+    def get_users(self):
+        self.file_exist()
+        with open(self.file, "r", encoding='utf-8') as f:
+            self.users = json.load(f)
+
+    def add_users(self, user_name: str, points: int):
+        if self.check_exist_user(user_name) is False:
+            self.users.append({
+                'user_name': user_name.upper(),
+                'points': points})
+            self.white_file()
+
+    def remove_user(self, user_name: str):
+        for user in self.users:
+            if user['user_name'] == user_name.upper():
+                self.users.remove(user)
+                self.white_file()
+
+    def plus_points(self, user_name: str, points: int):
+        if self.check_exist_user(user_name) is True:
+            for user in self.users:
+                if user['user_name'] == user_name.upper():
+                    user['points'] = user['points'] + points
+                    self.white_file()
+
+    def clear_users(self):
+        self.users = []
+        self.white_file()
+
+
+class Board():
+    def __init__(self, json_folder: str, json_game: str):
+        self.json_folder: str = json_folder
+        self.json_game: str = json_game
+        self.file: str = self.json_folder + self.json_game
+        self.active_place: int = 0
+        self.games: list = []
+        self.read_game()
+
+    def file_exist(self):
+        if os.path.exists(self.file):
+            pass
+        else:
+            self.white_file()
+
+    def white_file(self):
+        with open(self.file, "w", encoding='utf-8') as f:
+            json.dump(self.games, f)
+
+    def new_game(self, place: int = 1):
+        self.games = [place]
+        self.white_file()
+        self.active_place = place
+
+    def read_game(self):
+        self.file_exist()
+        with open(self.file, "r", encoding='utf-8') as f:
+            self.games = json.load(f)
+        self.active_place = self.games[0]
+
+    def change_place(self, place: int):
+        self.file_exist()
+        self.games = [place]
+        self.active_place = place
+        self.white_file()
+
+    def clear_game(self):
+        self.new_game(0)
